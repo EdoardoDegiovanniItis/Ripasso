d7b8157b48bf49bf58c2eb8aeda07a57a40946d3
Edoardo <edoardo.degiovanni@itiscuneo.eu>
2024-09-13 13:24:59 +0200

    Commenti

--- tabellina.py
+++ tabellina.py
@@ -1,2 +1,4 @@
-for i in range (4, 41, 4):      #si parte da 4, si arriva fino a 40 (considerando che l'ultimo num. nel range non lo trova quindi metto 41, il terzo numero sono gli step)
+#tabellina del 4
+#si parte da 4, si arriva fino a 40 (considerando che l'ultimo num. nel range non lo trova quindi metto 41, il terzo numero sono gli step)
+for i in range (4, 41, 4):      
     print(i)
